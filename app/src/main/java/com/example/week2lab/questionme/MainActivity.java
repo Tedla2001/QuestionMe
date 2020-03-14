13b5ea9c3cb0fd637af7b627524eec5bf2b95a21
Tedla <tedlamariye99@outlook.com>
2020-03-13 23:20:15 -0400

    Added the capability to add a flashcard

--- app/src/main/java/com/example/week2lab/questionme/MainActivity.java
+++ app/src/main/java/com/example/week2lab/questionme/MainActivity.java
@@ -7,14 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
 import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
+
         final TextView tv_answer = findViewById(R.id.flashcard_answer);
         final TextView tv_question = findViewById(R.id.flashcard_question);
         tv_answer.setVisibility(View.INVISIBLE);
@@ -39,16 +53,38 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent,100);
             }
         });
+
+        findViewById(R.id.arrow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            }
+        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
         super.onActivityResult(requestCode,resultCode,data);
+        allFlashcards = flashcardDatabase.getAllCards();
+
         if (resultCode== RESULT_OK) {
             String string1=data.getStringExtra("string1");
             String string2=data.getStringExtra("string2");
             ((TextView) findViewById(R.id.flashcard_question)).setText(string1);
             ((TextView) findViewById(R.id.flashcard_answer)).setText(string2);
+            flashcardDatabase.insertCard(new Flashcard(string1, string2));
+
         }
     }
+
 }
 
